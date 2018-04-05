b291d73dd2fb97aabab3eac56891ae570c39c408
Thiago Leite Luiz <tlluiz@gmail.com>
2018-04-05 15:22:52 -0300

    Create the new and create methods to topic controller.

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,11 +1,19 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
+  alias Discuss.Topic
 
-  def new(conn, params) do
-    IO.puts "++++++"
-    IO.inspect conn
-    IO.puts "++++++"
-    IO.inspect params
-    IO.puts "++++++"
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+    case Repo.insert(changeset) do
+      {:ok, post} ->
+
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
   end
 end
