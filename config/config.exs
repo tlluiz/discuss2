243c2a21f90b7e7f4e73833da8c67ad59f65e1e9
Thiago Leite Luiz <tlluiz@gmail.com>
2018-04-10 15:18:07 -0300

    Add ueberauth lib into the project.

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,12 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+config :ueberauth, Uberauth.Strategy.Github.Oauth,
+  client_id: "b666035d911980c051dd",
+  client_secret: "your-secret-key-goes-here"
