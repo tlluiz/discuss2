806d7863c9edfc2a7378636cbcb66a9161d258d2
Thiago Leite Luiz <tlluiz@gmail.com>
2018-04-12 18:52:30 -0300

    Creates the signin function into AuthController and the SetUser plug.

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -4,12 +4,35 @@ defmodule Discuss.AuthController do
 
   alias Discuss.User 
   
-  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do 
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do 
     user_params = %{ token: auth.credentials.token,
       email: auth.info.email,
       provider: "github"
     }
 
-    changeset = User.changeset(User, user_params)
+    changeset = User.changeset(%User{}, user_params)
+    signin(conn, changeset)
+  end
+
+  defp signin(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: topic_path(conn, :index))
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Error signing in")
+        |> redirect(to: topic_path(conn, :index))
+    end
+
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil -> Repo.insert(changeset)
+      user -> {:ok, user}
+    end
   end
 end
